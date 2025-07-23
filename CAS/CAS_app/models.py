f894ecc531932b8f35d31e72599abf6cec9f55f7
saadkhi <saadatherali1256@gmail.com>
2025-07-23 22:48:34 +0500

    model add

--- CAS/CAS_app/models.py
+++ CAS/CAS_app/models.py
@@ -7,3 +7,20 @@ class NewsletterSubscriber(models.Model):
 
     def __str__(self):
         return self.email
+
+# class user_form(models.Model):
+#     name = models.CharField(max_length=100)
+#     degree = models.CharField(max_length=100)
+#     institution = models.CharField(max_length=100)
+#     graduation_year = models.IntegerField()
+#     GPA = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+#     skills = models.TextField(blank=True, null=True)
+#     certifications = models.TextField(blank=True, null=True)
+#     resume_pdf = models.FileField(upload_to='resumes/', blank=True, null=True)
+
+#     # Resume analysis outcome
+#     score = models.IntegerField(blank=True, null=True)
+#     suggestions = models.TextField(blank=True, null=True)
+
+#     def __str__(self):
+#         return self.name
